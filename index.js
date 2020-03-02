d28a5aae19c27054dd8ec36f4bcddeb709bc0de7
liangpure <wsliangql@sina.com>
2020-03-02 17:51:18 +0800

    如果当前相册下载失败，确保下一个相册继续下载

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ log4js.configure({
   categories: { default: { appenders: ['cheese'], level: 'debug' } }
 });
 
-const logger = log4js.getLogger('cheese');
-const loggerConsole = log4js.getLogger();
-loggerConsole.level = 'debug'
+const logger = log4js.getLogger('record');
+logger.level = 'debug';
 
 
 async function getAttributeByElem(pageHandle, elemHandle, attrName) {
@@ -25,10 +24,10 @@ async function getAttributeByElem(pageHandle, elemHandle, attrName) {
 
 
 (async () => {
-  const browser = await puppeteer.launch({headless: true, slowMo: 250});
+  const browser = await puppeteer.launch({headless: false, slowMo: 250});
   const page = await browser.newPage();
   // 允许命令行直接输入QQ号 TODO TODO
-  await page.goto('https://user.qzone.qq.com/1475099525');
+  await page.goto('https://user.qzone.qq.com/644276847');
   // 需要先在电脑上登录QQ
   // 等待iframe加载完毕
   const frame = await page.frames().find(f => f.name() === 'login_frame');
@@ -39,42 +38,49 @@ async function getAttributeByElem(pageHandle, elemHandle, attrName) {
   await page.waitForNavigation({
     waitUntil: 'domcontentloaded'
   });
-  await page.waitForSelector("a[title='相册']");
-  await page.click('a[title="相册"]');
-  // 进入相册页面等待加载完成
-  await page.waitFor(1000);
-  await page.waitForSelector('#tphoto');
-  // 找到相册列表的iframe
-  const photoFrame = await page.frames().find(f => {
-    const identity = f.name();
-    return identity === 'app_canvas_frame' || identity === 'tphoto';
-  });
-  // 页面滚动到底部 加载相册
-  // await page.evaluate(_ => {
-  //   window.scrollBy(0, window.innerHeight);
-  // });
-  await page.evaluate(() => {
-    return new Promise((resolve, reject) => {
-        let totalHeight = 0;
-        let distance = 400;
-        let timer = setInterval(() => {
-            let scrollHeight = document.body.scrollHeight;
-            window.scrollBy(0, distance);
-            totalHeight += distance;
-            console.log('totalHeight, ', totalHeight, ' scrollHeight ', scrollHeight)
-            if(totalHeight >= scrollHeight){
-                clearInterval(timer);
-                resolve();
-            }
-        }, 500);
+  // 进入相册列表页面
+  let photoFrame;
+  async function getInAlbumsListPage() {
+    await page.waitForSelector(".homepage-link");
+    await page.hover("a.homepage-link")
+    await page.waitForSelector('.nav-drop-down');
+    await page.click('i.icon-album');
+    // 进入相册页面等待加载完成
+    await page.waitFor(1000);
+    await page.waitForSelector('#tphoto');
+    // 找到相册列表的iframe
+    const photoFrame = await page.frames().find(f => {
+      const identity = f.name();
+      return identity === 'app_canvas_frame' || identity === 'tphoto';
     });
-  });
-  
+    // 页面滚动到底部 加载相册
+    // await page.evaluate(_ => {
+    //   window.scrollBy(0, window.innerHeight);
+    // });
+    await page.evaluate(() => {
+      return new Promise((resolve, reject) => {
+          let totalHeight = 0;
+          let distance = 400;
+          let timer = setInterval(() => {
+              let scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+              console.log('totalHeight, ', totalHeight, ' scrollHeight ', scrollHeight)
+              if(totalHeight >= scrollHeight){
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 500);
+      });
+    });
+    
 
-  await page.waitFor(500);
-  // console.log(await page.frames());
-  await photoFrame.waitForSelector('.js-album-list-ul');
-  // console.log(photoFrame);
+    await page.waitFor(500);
+    // console.log(await page.frames());
+    await photoFrame.waitForSelector('.js-album-list-ul');
+    return photoFrame;
+  }
+  photoFrame = await getInAlbumsListPage();
   const albumList = [];
   let albumEles = await photoFrame.$$('.album-list .js-album-list-ul>li>div');
   // 获取相册列表，目前还没考虑相册列表有分页的情况... TODO TODO
@@ -99,24 +105,6 @@ async function getAttributeByElem(pageHandle, elemHandle, attrName) {
     await elementHandle.click();
     // await albumInfo.elementHandle.click();
     await photoFrame.waitForSelector('.list.j-pl-photolist-ul');
-
-    // let pictureElems = await photoFrame.$$('.mod-photo-list li.j-pl-photoitem>div');
-    // // console.log(pictureElems);
-    // let pictures = Array.from(pictureElems);
-    // const pictureList = [];
-    // for (let j = 0; j < pictures.length; j++) {
-    //   const picElemHandle = pictures[j];
-    //   const titleSpanElem = await picElemHandle.$('.item-tit>span');
-    //   const name = await getAttributeByElem(photoFrame, titleSpanElem, 'title');
-    //   pictureList.push({
-    //     name,
-    //     albumName: albumInfo.name,
-    //     elementHandle: picElemHandle,
-    //     order: j,
-    //     isLastOne: j === (pictures.length - 1)
-    //   })
-    // }
-    
   
     let pictureElem = await photoFrame.$('.mod-photo-list li.j-pl-photoitem>div');
     // 点击第一个图片 弹出显示照片的框
@@ -131,7 +119,7 @@ async function getAttributeByElem(pageHandle, elemHandle, attrName) {
         })
       } catch(err) {
         console.error(err);
-        logger.trace(`相册${albumInfo.name}第${i+1}张图片下载失败`);
+        logger.info(`相册${albumInfo.name}第${i+1}张图片下载失败`);
       }
     }
     // 当前相册下载完毕以后，退出当前相册
@@ -186,9 +174,9 @@ async function getAttributeByElem(pageHandle, elemHandle, attrName) {
       if(err) {
           return console.log(err);
       }
-      const info = `${albumInfo.name}第${picInfo.order+1}张图片已经被保存`;
-      loggerConsole.info(info)
-      logger.info(info);
+      // const info = `${albumInfo.name}第${picInfo.order+1}张图片已经被保存`;
+      // loggerConsole.info(info)
+      // logger.info(info);
     });
   }
   async function downloadTheAlbumPictures(albumInfo) {
@@ -203,20 +191,16 @@ async function getAttributeByElem(pageHandle, elemHandle, attrName) {
         // 悬浮在元素上 然后点击下一张
         await page.hover('#js-image-ctn');
         await page.waitFor(200);
-        if (i === (albumInfo.pictureNum - 1)) await page.click('#js-btn-nextPhoto');
+        if (i !== (albumInfo.pictureNum - 1)) await page.click('#js-btn-nextPhoto');
       } catch(err) {
         console.error(err);
-        logger.trace(`相册${albumInfo.name}第${i+1}张图片下载失败`);
+        logger.info(`相册${albumInfo.name}第${i+1}张图片下载失败`);
       }
     }
   }
   async function getOutAlbum() {
     // 当前相册下载完毕以后，退出当前相册
-    try {
-      await page.$('.photo_layer_close').then((handle) => handle.click('.photo_layer_close'));
-    } catch (err) {
-      console.error(err);
-    }
+    await page.$('.photo_layer_close').then((handle) => handle.click('.photo_layer_close'));
     await photoFrame.click('li[data-mod="albumlist"]');
     await photoFrame.waitForSelector('.js-album-list');
   }
@@ -229,15 +213,9 @@ async function getAttributeByElem(pageHandle, elemHandle, attrName) {
         await getOutAlbum();
       } catch(err) {
         console.error(err);
-        logger.trace(`相册${albumInfo.name}下载失败`);
-        // 当前相册下载完毕以后，退出当前相册
-        // try {
-        //   await page.$('.photo_layer_close').then((handle) => handle.click('.photo_layer_close'));
-        // } catch(err) {
-        //   console.error(err);
-        // }
-        // await photoFrame.click('li[data-mod="albumlist"]');
-        // await photoFrame.waitForSelector('.js-album-list');
+        logger.info(`相册${albumInfo.name}下载失败`);
+        // 如果之前一个相册下载失败，重新进入相册页面
+        await getInAlbumsListPage();
       }
     }
   }
